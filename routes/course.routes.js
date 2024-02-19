a6734a7d9062513a5517be3b89efc841cbed7779
Hrodriguez2019188 <hrodriguez-2019188@kinal.edu.gt>
2024-02-18 22:35:23 -0600

    se estructuro cursos

--- routes/course.routes.js
+++ routes/course.routes.js
@@ -3,18 +3,50 @@ const { check } = require('express-validator');
 
 const { validarCampos } = require('../middlewares/validar-campos');
 
-const { cursoPost  } = require('../controllers/cursos.controller');
-
+const { createCourse, getCourses, getCourseById, updateCourse, deleteCourse } = require('../controllers/course.controller');
 
 const router = Router();
 
 router.post(
-    "/", 
-    [
-        check("nombreC","El nombre es obligatorio").not().isEmpty(),
-        check("profesorC","El profesor es obligatorio").not().isEmpty(),
-        validarCampos,
-    ], cursoPost); 
+  '/',
+  [
+    check('courseName', 'Course name is required').not().isEmpty(),
+    check('courseTeacher', 'Course teacher is required').not().isEmpty(),
+    validarCampos,
+  ],
+  createCourse
+);
 
+router.get('/', getCourses);
+
+router.get(
+  '/:id',
+  [
+    check('id', 'Invalid id').isMongoId(),
+    check('id').custom(courseExistsById),
+    validarCampos,
+  ],
+  getCourseById
+);
+
+router.put(
+  '/:id',
+  [
+    check('id', 'Invalid id').isMongoId(),
+    check('id').custom(courseExistsById),
+    validarCampos,
+  ],
+  updateCourse
+);
+
+router.delete(
+  '/:id',
+  [
+    check('id', 'Invalid id').isMongoId(),
+    check('id').custom(courseExistsById),
+    validarCampos,
+  ],
+  deleteCourse
+);
 
 module.exports = router;
